117ab23d0dec7272d8a09287ddc6b2199578bbd7
Ankit Yadav <ankityadav60062@gmail.com>
2024-07-19 21:47:02 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -24,14 +24,18 @@ function App() {
       <div className='text-[#0398d4] font-medium text- 2xl'>Increment & Decrement</div>
       <div className='bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]'>
         
-         <button onClick={decHandler} className='border-r-2 text-center w-20 border-[#bfbfbf] text-5xl'>-</button> 
+         <button onClick={decHandler} className='border-r-2 text-center w-20 border-[#bfbfbf] text-5xl'>
+          -
+          </button> 
         
         <div className='font-bold gap-12 text-5xl'>
 {count}
 </div>
         
          
-          <button onClick={incHandler} className='border-r-2 text-center w-20 border-[#bfbfbf] text-5xl'>+</button>
+          <button onClick={incHandler} className='border-r-2 text-center w-20 border-[#bfbfbf] text-5xl'>
+            +
+            </button>
          </div>
          
           <button onClick={resetHandler} className='bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg '>reset</button>
